f10e2a5cddbc265115faefe857acf5719a245c9d
ETarifa <elverson435@gmail.com>
2024-03-23 11:51:56 -0300

    Organizando os arquivos

---
+++ infra/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-06aa3f7caf3a30282"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+#  user_data = <<-EOF
+#                 #!/bin/bash
+#                 cd /home/ubuntu
+#                 echo "<h1>Feito com Terraform</h1>" > index.html
+#                 nohup busybox httpd -f -p 8080 &
+#                 EOF
+  tags = {
+    Name = "Terraform Ansible Python"
+  }
+}
+
+resource "aws_key_pair" "chaveSSH"{
+  key_name = DEV
+  public_key = file("IaC-DEV.pub")
+
+}
