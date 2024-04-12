bb635468676da018ea83daec80b0650f3cc334a6
ETarifa <elverson435@gmail.com>
2024-04-12 17:56:34 -0300

    Idempotência e GitClone

--- env/Prod/main.tf
+++ env/Prod/main.tf
@@ -1,6 +1,10 @@
 module "aws-prod" {
     source = "../../infra"
-    instancia = "t2.small"
+    instancia = "t2.micro"
     regiao_aws = "us-east-1"
     chave = "IaC-Prod"
+}
+
+output "IP" {
+    value = module.aws-prod.IP_publico
 }
